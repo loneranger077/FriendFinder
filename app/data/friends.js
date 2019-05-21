1eeb9a2282e9082314c60e05b261c4fcfbacc09c
nguzak3 <nick.guzak@gmail.com>
2019-05-21 19:23:37 -0400

    more basic setup

---
+++ app/data/friends.js
@@ -0,0 +1,178 @@
+[ 
+    {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+   },
+   {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Lou Ritter",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Jordan Biason",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "tabitha",
+    "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F248797%2Fpexels-photo-248797.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D1%26w%3D500&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fnature%2F&docid=ShwNVOdFBcmkxM&tbnid=8c_UAo3gH_220M%3A&vet=10ahUKEwiG1Me6kK3iAhWuY98KHdU9AaUQMwh6KAAwAA..i&w=500&h=200&bih=657&biw=1366&q=images&ved=0ahUKEwiG1Me6kK3iAhWuY98KHdU9AaUQMwh6KAAwAA&iact=mrc&uact=8",
+    "scores": [
+    "5",
+    "4",
+    "2",
+    "1",
+    "2",
+    "4",
+    "3",
+    "3",
+    "2",
+    "3"
+    ]
+},
+{
+    "name": "Jim",
+    "photo": "https://tse1.mm.bing.net/th?id=OIP.zeMVy2zuPfPhqDlVPB8r-gHaEK&pid=Api&rs=1&c=1&qlt=95&w=169&h=95",
+    "scores": [
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3"
+    ]
+    },
+    {
+    "name": "Sofia Luna",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiezNXG2K3iAhWE1lkKHVvzClcQjRx6BAgBEAU&url=https%3A%2F%2Fwww.petfinder.com%2Fcats%2F&psig=AOvVaw2-mDnkCPx5qoiFzvtte9Df&ust=1558564822424540",
+    "scores": [
+    "3",
+    "4",
+    "1",
+    "3",
+    "4",
+    "1",
+    "2",
+    "5",
+    "5",
+    "2"
+    ]
+    },
+    {
+        "name": "Yue",
+        "photo": "https://www.google.com/search?rlz=1C1GCEA_enUS778US778&biw=1920&bih=937&tbm=isch&sa=1&ei=ulrkXMwu1J76BKn7n2g&q=thor&oq=thor&gs_l=img.3..35i39l2j0l8.718.10440..10648...3.0..1.102.669.7j1......3....1..gws-wiz-img.....0..0i10i24j0i24.WAAt-O2mJpE#imgrc=nLagc7prJV6i8M:",
+        "scores": [
+        "5",
+        "4",
+        "4",
+        "3",
+        "5",
+        "2",
+        "2",
+        "3",
+        "3",
+        "4"
+        ]
+        },
+        {
+            "name": "Amit Kumar",
+            "photo": "/Users/amits/Desktop/MyDesktop/MacDesktop/Pic/IMG_8642.JPG",
+            "scores": [
+            "4",
+            "4",
+            "5",
+            "4",
+            "4",
+            "2",
+            "4",
+            "4",
+            "4",
+            "3"
+            ]
+            },
+]
