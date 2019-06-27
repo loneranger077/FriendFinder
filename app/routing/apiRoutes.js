177a1da68bc93e82bbb19062b70cb546a8817ef4
nguzak3 <nick.guzak@gmail.com>
2019-06-27 15:11:44 -0400

    some more fixes, mainly paths

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,28 +1,27 @@
-const app = express();
+var friends = require("../data/friends.js")
 
 module.exports = function(app) {
 
-app.post("/api/friends",  function(req, res) {
+app.post("./api/friends",  function(req, res) {
     var newRes = req.body;
     console.log(newRes);
     var score = [];
     for (let i=0; i<10; i++) {
-        score = score.append(newRes.scores[i]);
+        score = score.push(newRes.scores[i]);
     };
     const diff_tots = [];
     for (let i=0; i<11; i++) {
         var diff_tot;
         for (let j=0; j<10; j++) {
-            const diff = Math.abs((friends.js[i].scores[j])-(score[j]));
+            const diff = Math.abs((friends[i].scores[j])-(score[j]));
             diff_tot = diff_tot + diff;
         };
-        diff_tots = diff_tots.append(diff_tot);
+        diff_tots = diff_tots.push(diff_tot);
     };
     match_num = Array.min(diff_tots);
     name = friends.js[match_num].name;
     url = friends.js[match_num].photo;
-    $("#match").modal(`Your match is: ${name}`);
-    res.json(newRes);
+    res.json(friends[0]);
 });
 
 app.get("/api/friends", function(req, res) {
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const path = require("path");
+var app = express();
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
-require("./app/data/friends.js")(app);
 
-var app = express();
 var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
